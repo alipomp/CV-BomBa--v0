b299d89a210e6e780d3878f2ed4e716600db158c
BomBa <73615643+alipomp@users.noreply.github.com>
2022-11-14 16:38:11 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <img class="BomBa__logo" alt="Vue logo" src="./assets/logo.jpg" />
+  <HelloWorld msg="Welcome to BomBa Software." />
+</template>
+
+<script>
+import HelloWorldVue from "./components/HelloWorld.vue";
+export default {
+  name: "App",
+  components: {
+    HelloWorld: HelloWorldVue,
+  },
+};
+</script>
+
+<style lang="scss">
+$c00: #efefef;
+$c60: #909090;
+$c30: #2c3e50;
+$c10: #f18486;
+body {
+  background: $c60;
+  color: $c00;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  /* color: $c30; */
+  margin-top: 60px;
+}
+.BomBa__logo {
+  padding: 5px;
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  border: 3px solid $c30;
+  box-shadow: 0px 0px 20px $c10;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h3>he I'm hussein abdo </h3>
+    <p>My job is a <h4 style="display: inline;">full stack web developer</h4></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+</style>
